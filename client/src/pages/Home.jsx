69d8c266058cee4147f17eb9c3950856c1a185b4
Sardar Nazeer <sardarnazeer1108@gmail.com>
2026-07-07 16:18:10 +0500

    made some more components of the website

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedsDestination from '../components/FeaturedsDestination'
+import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonials from '../components/Testimonials'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedsDestination/>
+      <ExclusiveOffer/>
+      <Testimonials/>
+      <NewsLetter/>
+      <Footer/>
     </>
   )
 }
